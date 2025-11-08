6bb8ea0577c37032514ba43d87f1142d68ef600f
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2025-11-08 13:21:08 +0800

    types

--- TownPass-Services/src/views/TestView.vue
+++ TownPass-Services/src/views/TestView.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+type DataItem = {
+  name: string;
+  swimPeopleNum: number;
+  swimPeopleNumMax: number;
+  gymPeopleNum: number;
+  gymPeopleNumMax: number;
+  latitude: number;
+  longitude: number;
+};
+const data = ref<DataItem[]>([]);
+const loading = ref(false);
+const error = ref<string | null>(null);
+</script>
 <template>
   <main>
     <div class="py-4">
